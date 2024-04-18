ea0888544ceaafd21d0a422e9bb1b194cfdfe2cf
Aleksandra PLatanaya <platanaya48@gmail.com>
2024-04-18 13:21:18 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вычислить значение формулы (a*b)/(c+d), где a,b,c,d - некоторые целые числа.Результат вывести на экран.
+
+int a = 2;
+int b = 3;
+int c = 1;
+int d = 1;
+
+int P = a * b;
+int S = c + d;
+int Z = P / S;
+Console.WriteLine(P);
+Console.WriteLine(S);
+Console.WriteLine(Z);
